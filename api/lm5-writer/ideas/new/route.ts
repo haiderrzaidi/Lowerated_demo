b9dce5cb8c0244a165b60323ce5d6147c977c94b
haiderrzaidi <zaidihaider336@gmail.com>
2024-10-08 10:54:33 +0500

    Updated Endpoint

--- api/lm5-writer/ideas/new/route.ts
+++ api/lm5-writer/ideas/new/route.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const sendMessageToAPI = async (message: string, history: Array<{ type: string; text: string }>): Promise<string> => {
   try {
-    console.log("History: ", history);    
-    const response = await axios.post('https://d3a1-13-60-20-160.ngrok-free.app/chat', {
+    // console.log("History: ", history);    
+    const response = await axios.post('https://360f-13-60-20-160.ngrok-free.app/chat', {
       message,
       history,  // Pass history as an array
     });
